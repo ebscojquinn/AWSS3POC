faf756e339e5c5038246029e3ead35e8b0d8735e
ebscojquinn <jquinn@ebsco.com>
2018-01-05 08:07:29 -0600

    Initial Commit

---
+++ AWSS3POC/Program.cs
@@ -0,0 +1,55 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.S3.Transfer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWSS3POC
+{
+    class Program
+    {
+        static string bucketName = "eis-opf-docgen-test";
+        static string filePath = "C:/Code/test.csv";
+
+        public static void Main(string[] args)
+        {
+            WritingAnObject();
+        }
+
+        static void WritingAnObject()
+        {
+            try
+            {
+
+                TransferUtility fileTransferUtility = new
+                    TransferUtility(new AmazonS3Client(Amazon.RegionEndpoint.USEast2));
+
+                // 1. Upload a file, file name is used as the object key name.
+                fileTransferUtility.Upload(filePath, bucketName);
+                Console.WriteLine("Upload completed");
+               
+            }
+            catch (AmazonS3Exception amazonS3Exception)
+            {
+                if (amazonS3Exception.ErrorCode != null &&
+                    (amazonS3Exception.ErrorCode.Equals("InvalidAccessKeyId")
+                    ||
+                    amazonS3Exception.ErrorCode.Equals("InvalidSecurity")))
+                {
+                    Console.WriteLine("Check the provided AWS Credentials.");
+                    Console.WriteLine(
+                        "For service sign up go to http://aws.amazon.com/s3");
+                }
+                else
+                {
+                    Console.WriteLine(
+                        "Error occurred. Message:'{0}' when writing an object"
+                        , amazonS3Exception.Message);
+                }
+            }
+        }
+    }
+}
